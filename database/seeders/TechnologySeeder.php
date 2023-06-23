3c2ffb7c1f8373ec6e9e4aee5530ba7b7fc5f10f
gluke0 <milanesigianluca@gmail.com>
2023-06-23 15:16:47 +0200

    technologySeeder data

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Admin\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class TechnologySeeder extends Seeder
 {
@@ -14,6 +16,13 @@ class TechnologySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $technologies = ['CSS', 'HTML', 'Laravel', 'JavaScript', 'Vue', 'Vite', 'PHP', ];
+
+        foreach($technologies as $language){
+            $new_technology = new Technology();
+            $new_technology->name = $language;
+            $new_technology->slug = Str::slug($new_technology->name, '-');
+            $new_technology->save();
+        }
     }
 }
