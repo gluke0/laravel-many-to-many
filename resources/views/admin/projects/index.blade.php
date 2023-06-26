d7ca646f3c0a895166cacacd4d16244ca6207248
gluke0 <milanesigianluca@gmail.com>
2023-06-26 17:18:13 +0200

    some graphic adjustments

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -3,41 +3,40 @@
 @section('title', 'gluke | Projects')
 
 @section('content')
-    <h1 class="text-uppervase"> my projects </h1>
+    <div class="container">
+        <h1 class="text-uppervase"> my projects </h1>
 
-    @if (Session::has('success'))
-        <div>
-            {!! Session::get('success') !!}
-        </div>
-    @endif
-
+        @if (Session::has('success'))
+            <div>
+                {!! Session::get('success') !!}
+            </div>
+        @endif
     </div>
+    <div class="container">
         <button class="text-uppercase btn btn-primary"><a class="text-uppercase text-white text-decoration-none" href="{{ route('admin.projects.create') }}">add project</a></button>
-    </div>
+        @forelse ($projects as $elem)
+        <div>
+            <div class="card-body card p-3">
+                
+                <p class="card-text"><strong> Title: </strong> {{ $elem->title }} </p>
 
-    @forelse ($projects as $elem)
-    <div class="card">
-        <div class="card-body p-3">
+                <p class="card-text"> <img class="img-fluid" src="{{asset('storage/' . $elem->image)}}" alt=""> </p>
+
+                @if ($elem->technologies && count($elem->technologies) > 0)
+                    <p class="card-text"><strong> Technology: </strong> 
+                        @foreach ($elem->technologies as $item)
+                            {{$item->name}}
+                        @endforeach
+                    </p>
+                @else
+                    <p class="card-text"><strong> Technology: </strong> <span class="text-danger"> Project techology has not been declared </span></p>
+                @endif
+
+                <p> <a class="text-decoration-none" href="{{ route('admin.projects.show', $elem) }}"> More </a></p>
+            </div>
             
-            <p class="card-text"><strong> Title: </strong> {{ $elem->title }} </p>
-
-            <p class="card-text"> <img class="img-fluid" src="{{asset('storage/' . $elem->image)}}" alt=""> </p>
-
-            @if ($elem->technologies && count($elem->technologies) > 0)
-                <p class="card-text"><strong> Technology: </strong> 
-                    @foreach ($elem->technologies as $item)
-                        {{$item->name}}
-                    @endforeach
-                </p>
-            @else
-                <p class="card-text"><strong> Technology: </strong> <span class="text-danger"> Project techology has not been declared </span></p>
-            @endif
-
-            <p> <a class="text-decoration-none" href="{{ route('admin.projects.show', $elem) }}"> More </a></p>
-        </div>
-        
-    @empty
-        
-    @endforelse
-
+        @empty
+            
+        @endforelse
+    </div>
 @endsection
--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -3,50 +3,51 @@
 @section('title', 'gluke | Project')
 
 @section('content')
-    <h1 class="text-uppervase"> {{ $project -> title }} </h1>
+    <div class="container frosted">
+        <h1 class="text-uppervase"> {{ $project -> title }} </h1>
 
-    <div class="card">
-        <div class="card-body p-3">
-            <p class="card-text"><strong> Image: </strong> <p class="card-text"> <img class="img-fluid" src="{{asset('storage/' . $project->image)}}" alt=""> </p> </p>
-            <p class="card-text"><strong> Description: </strong> {{ $project->description }} </p>
+        <div>
+            <div class="card-body p-3">
+                <p class="card-text"><p class="card-text"> <img class="img-fluid" src="{{asset('storage/' . $project->image)}}" alt=""> </p> </p>
+                <p class="card-text"><strong> Description: </strong> {{ $project->description }} </p>
 
-            @if ($project->technologies && count($project->technologies) > 0)
-                <p class="card-text"><strong> Technology: </strong> 
-                    @foreach ($project->technologies as $elem)
-                        {{ $elem->name }}
-                    @endforeach
-                </p>
-            @else
-                <p class="card-text"><strong> Technology: </strong> <span class="text-danger"> Project techology has not been declared </span></p>
-            @endif
+                @if ($project->technologies && count($project->technologies) > 0)
+                    <p class="card-text"><strong> Technology: </strong> 
+                        @foreach ($project->technologies as $elem)
+                            {{ $elem->name }}
+                        @endforeach
+                    </p>
+                @else
+                    <p class="card-text"><strong> Technology: </strong> <span class="text-danger"> Project techology has not been declared </span></p>
+                @endif
 
-            {{-- this is possible because of the controller and the relationship created in the Project+Category.php files --}}
-            {{-- <p class="card-text"><strong> Category: </strong> {{ $project->category->name }} </p> --}}
+                {{-- this is possible because of the controller and the relationship created in the Project+Category.php files --}}
+                {{-- <p class="card-text"><strong> Category: </strong> {{ $project->category->name }} </p> --}}
 
-            @if ($project->category != null)
-                <p class="card-text"><strong> Category: </strong> {{ $project->category->name }}</p>
-            @else
-                <p class="card-text"><strong> Category: </strong> <span class="text-danger"> This project has no category </span></p>
-            @endif
-            @if ($project->link != null)
-                <p class="card-text"><a target=”_blank” href="{{ $project->link }}"> source code </a></p>
-            @else
-                <p class="card-text"> <span class="text-danger"> This project has no download link </span></p>
-            @endif
+                @if ($project->category != null)
+                    <p class="card-text"><strong> Category: </strong> {{ $project->category->name }}</p>
+                @else
+                    <p class="card-text"><strong> Category: </strong> <span class="text-danger"> This project has no category </span></p>
+                @endif
+                @if ($project->link != null)
+                    <p class="card-text"><a target=”_blank” href="{{ $project->link }}"> source code </a></p>
+                @else
+                    <p class="card-text"> <span class="text-danger"> This project has no download link </span></p>
+                @endif
+            </div>
         </div>
-    </div>
-    <div class="mt-5">
-        <a href=" {{ route( 'admin.projects.edit', $project ) }} " class="btn btn-warning text-danger text-uppercase"><strong> edit</strong></a>
-    </div>
-    <div class="mt-2">
-        <form action="{{route('admin.projects.destroy', $project)}}" method="POST">
+        <div class="mt-5">
+            <a href=" {{ route( 'admin.projects.edit', $project ) }} " class="btn btn-warning text-danger text-uppercase"><strong> edit</strong></a>
+        </div>
+        <div class="mt-2">
+            <form action="{{route('admin.projects.destroy', $project)}}" method="POST">
 
-        @csrf
-        @method('DELETE')
+            @csrf
+            @method('DELETE')
 
-        <button type="submit" class="btn btn-danger text-uppercase text-warning" onclick="return confirm('Are you sure you want to delete {{$project -> title }}')"> <strong> delete </strong> </button>
+            <button type="submit" class="btn btn-danger text-uppercase text-warning" onclick="return confirm('Are you sure you want to delete {{$project -> title }}')"> <strong> delete </strong> </button>
 
-        </form>
-    </div>
-
+            </form>
+        </div>
+</div>
 @endsection
